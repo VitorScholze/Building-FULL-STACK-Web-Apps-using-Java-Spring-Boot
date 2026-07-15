ad86217b6ec038ea7f3cf43a6b1147d2c1c16fa0
Vitor Scholze <vitorscholze7@gmail.com>
2026-07-15 19:22:44 -0300

    delete button added and API configured

--- Curso FULL-STACK Web, Spring boot/ems-frontend/src/components/ListDepartmentsComponent.jsx
+++ Curso FULL-STACK Web, Spring boot/ems-frontend/src/components/ListDepartmentsComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { getAllDepartments } from '../services/DepartmentService';
+import { deleteDepartment, getAllDepartments } from '../services/DepartmentService';
 import { Link, useNavigate} from 'react-router-dom';
 
 export const ListDepartmentsComponent = () => {
@@ -9,16 +9,31 @@ export const ListDepartmentsComponent = () => {
      const navigator = useNavigate();
 
     useEffect(() => {
-        getAllDepartments().then((response) =>{
+       listOfDepartments();
+    },[])
+        
+    function updateDepartment(id){
+       navigator(`/edit-department/${id}`)
+    }
+
+
+    function listOfDepartments(){
+         getAllDepartments().then((response) =>{
             console.log(response.data);
             setDepartments(response.data)
         }).catch(error => {
             console.error(error)
         })
-    },[])
-        
-    function updateDepartment(id){
-       navigator(`/edit-department/${id}`)
+    }
+
+
+    function removeDepartment(id){
+        deleteDepartment(id).then((response) => {
+            console.log(response.data)
+            listOfDepartments();
+        }).catch(error => {
+            console.error(error);
+        })
     }
 
   return (
@@ -43,7 +58,9 @@ export const ListDepartmentsComponent = () => {
                             <td>{department.departmentDescription}</td>
                             <td>
                                 <button onClick={() => updateDepartment(department.id)} className = "btn btn-info">Update</button>
-                                </td>
+                                <button onClick={() => removeDepartment(department.id)} className = "btn btn-danger" 
+                                style = {{marginLeft: "10px"}}>Delete</button>
+                            </td>
                         </tr>
                     )
                 }
