189ad940fa881ef569bd1c678827ad1a06e66e9b
Vitor Scholze <vitorscholze7@gmail.com>
2026-07-16 12:17:22 -0300

    relationship manyToOne added in React Apliccation

--- Curso FULL-STACK Web, Spring boot/ems-frontend/src/components/EmployeeComponent.jsx
+++ Curso FULL-STACK Web, Spring boot/ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,19 +1,32 @@
 import React, {useState, useEffect} from 'react'
 import { createEmployee, getEmployee, updateEmployee } from '../services/EmployeeService'
 import {useNavigate, useParams} from "react-router-dom"
+import { getAllDepartments } from '../services/DepartmentService'
 
 const EmployeeComponent = () => {
 
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
+    const [departmentId, setDepartmentId] = useState("")
+    const [departments, setDepartments] = useState([])
+
+
+    useEffect(() => {
+        getAllDepartments().then((response) => {
+            setDepartments(response.data)
+        }).catch(error => {
+            console.error(error);
+        })
+    },[])
 
     const {id} = useParams();
 
     const [errors, setErrors] = useState({
         firstName: "",
         lastName: "",
-        email : ""
+        email : "",
+        departments : ""
     })
 
     const navigator = useNavigate();
@@ -24,6 +37,7 @@ const EmployeeComponent = () => {
                 setFirstName(response.data.firstName);
                 setLastName(response.data.lastName);
                 setEmail(response.data.email);
+                setDepartmentId(response.data.departmentId)
             }).catch(error =>{
                 console.error(error);
             })
@@ -36,7 +50,7 @@ const EmployeeComponent = () => {
         e.preventDefault();
         if(validateForm()){
 
-            const employee = {firstName, lastName, email}
+            const employee = {firstName, lastName, email, departmentId}
             console.log(employee)
 
             if(id){
@@ -85,6 +99,13 @@ const EmployeeComponent = () => {
         }else{
             errorsCopy.email = "Email is Required"
             valid = false;
+        }
+
+        if(departmentId){
+            errorsCopy.departments = ""
+        }else{
+            errorsCopy.department = "Select Department"
+            valid = false
         }
 
         setErrors(errorsCopy)
@@ -150,6 +171,25 @@ const EmployeeComponent = () => {
                             >
                             </input>
                             {errors.email && <div className = "invalid-feedback">{errors.email}</div>}
+                        </div>
+
+                            <div className = "form-group mb-2">
+                            <label className = "form-label">Select Department:</label>
+                            <select
+                                className ={`form-control ${errors.department ? "is-invalid" : " "}`}
+                                value = {departmentId}
+                                onChange = {(e) => setDepartmentId(e.target.value)}
+                            >
+                                <option value = "Select Department">Select Department</option>
+                                {
+                                    departments.map(department => 
+                                        <option key = {department.id} value = {department.id}>{department.departmentName}</option>
+                                    )
+                                }
+
+                            </select>
+                           
+                            {errors.department && <div className = "invalid-feedback">{errors.department}</div>}
                         </div>
                         <button className = "btn btn-success" onClick={saveOrUpdateEmployee}>Submit</button>
                     </form>
